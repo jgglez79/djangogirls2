535bfe03259ac18e133d22309fcdefdf0f88b7c4
jgglez79 <jgglez79@gmail.com>
2016-01-15 08:58:01 +0000

    Corrección de sintaxis.

--- blog/tests.py
+++ blog/tests.py
@@ -5,7 +5,8 @@ import datetime
 
 from django.test import TestCase
 
-  class QuestionMethodTests(TestCase):
+
+class QuestionMethodTests(TestCase):
 
     def test_was_published_recently_with_future_question(self):
         """
